70630299338182ccc622b9332478ba6c6e46ee42
cesar laurino <cesarlaurino@hotmail.com>
2023-01-17 12:56:26 -0300

    intento hacer funcional el boton.favorito

--- index.js
+++ index.js
@@ -13,7 +13,8 @@ const body = document.querySelector (".modo-claro")
 /////////////////////BUSCADOR//////////////////////////////
 const buscadorPokemon = document.querySelector (".buscador")
 /////////////////////POKEMONS//////////////////////////////
-const contenedorPokemon = document.querySelector (".contenedor-pokemon")
+const contenedorPokemon =  document.querySelector (".contenedor-pokemon")
+const botonFavorito = document.querySelectorAll (".boton-fav")
 const arraypkm = []
 const card = document.querySelector (".card")
 // const botonFav = document.getElementByClassName(".boton-fav")
@@ -23,9 +24,9 @@ const spinner = document.querySelector (".wobbling-10")
 const paginacion = document.querySelector (".paginacion")
 const anterior = document.querySelector ("#anterior")
 const siguiente = document.querySelector ("#siguiente")
-const paginacion2 = document.querySelector (".paginacion2")
-const anterior2 = document.querySelector ("#anterior2")
-const siguiente2 = document.querySelector ("#siguiente2")
+// const paginacion2 = document.querySelector (".paginacion2")
+// const anterior2 = document.querySelector ("#anterior2")
+// const siguiente2 = document.querySelector ("#siguiente2")
 
 let offset = 1
 let limit = 2
@@ -40,54 +41,37 @@ function removeChildNodes (parent){
 
 anterior.addEventListener('click', () => {
     if (offset != 1)  {
-        offset -= 50;
+        offset -= 3;
         removeChildNodes(contenedorPokemon)
-        contenedorPokemon.innerHTML = "";
+        
         fetchPokemons (offset, limit);
+        contenedorPokemon.innerHTML = "";
     }
 })
 
 siguiente.addEventListener('click', () => {
-    offset += 50;
+    offset += 3;
     removeChildNodes(contenedorPokemon)
-    contenedorPokemon.innerHTML = "";
+
     fetchPokemons (offset, limit);
+    contenedorPokemon.innerHTML = "";
 }) 
 
-anterior2.addEventListener('click', () => {
-    if (offset != 1)  {
-        offset -= 50;
-        removeChildNodes(contenedorPokemon)
-        contenedorPokemon.innerHTML = "";
-        fetchPokemons (offset, limit);
-    }
-})
+// anterior2.addEventListener('click', () => {
+//     if (offset != 1)  {
+//         offset -= 50;
+//         removeChildNodes(contenedorPokemon)
+//         contenedorPokemon.innerHTML = "";
+//         fetchPokemons (offset, limit);
+//     }
+// })
 
-siguiente2.addEventListener('click', () => {
-    offset += 50;
-    removeChildNodes(contenedorPokemon)
-    contenedorPokemon.innerHTML = "";
-    fetchPokemons (offset, limit);
-}) 
-
-function fetchPokemon (id){
-
-    fetch ( `https://pokeapi.co/api/v2/pokemon/${id}/` )
-    .then (res => res.json())
-    .then (data =>{ 
-        arraypkm.push (data);
-        cardHtml(arraypkm)
-        })
-        spinner.style.display = "none"
-    }
-
-    function fetchPokemons (offset, limit){
-    spinner.style.display = "block"
-    for (let i = offset; i <= offset + limit; i++){
-        fetchPokemon(i);
-    }
-}
-
+// siguiente2.addEventListener('click', () => {
+//     offset += 50;
+//     removeChildNodes(contenedorPokemon)
+//     contenedorPokemon.innerHTML = "";
+//     fetchPokemons (offset, limit);
+// }) 
 
 //////////////////////////Crear Card Pokemons///////////////////////////
 
@@ -102,9 +86,9 @@ function cardHtml (data) {
                     </div>
                     <h2 class="numero">${u.id.toString().padStart(3, 0)}</h2>
                     <h2 class="nombre">${u.name}</h2>
+                    <button id="boton-${u.id}" class="boton-fav"></button>
                 </div>
             </div>
-            <button id="boton-${u.id}" class="boton-fav"></button>
             <div class="carta-atras">
                 <div class="contenido">
                 </div>
@@ -112,90 +96,47 @@ function cardHtml (data) {
         </div>
     `
     }, "")
-    
+
     contenedorPokemon.innerHTML = cartapkm
 
-    // .onclick = () => {
-    //     Toastify({
-    //         text: `Atrapaste un ${u.name}`,
-    //         duration: 1000,
-    //         className: "info",
-    //         close: true
-    //     }).showToast ()}
+    botonFavorito.onclick = () => {
+        Toastify({
+            text: `Atrapaste un ${u.name}`,
+            duration: 1000,
+            className: "info",
+            close: true
+        }).showToast ()}
 }
 
+function moverAlCarrito (){
+
+    botonFavorito.forEach (elemento => {
+        elemento.onclick = () => {
+            console.log (elemento.id)
+        }
+    })
+}
+moverAlCarrito()
 
 //////////////////////////LLamarlos desde api///////////////////////////
 
+function fetchPokemon (id){
 
+    fetch ( `https://pokeapi.co/api/v2/pokemon/${id}/` )
+    .then (res => res.json())
+    .then (data =>{ 
+        arraypkm.push (data);
+        cardHtml(arraypkm)
+        })
+        spinner.style.display = "none"
+    }
 
-// function crearPokemons(pokemon){
-    // const flipCard = document.createElement (`div`);
-    // flipCard.classList.add ("flip-card");
-
-    // const contenedorCard = document.createElement (`div`);
-    // contenedorCard.classList.add ("contenedor-card");
-
-    // flipCard.appendChild (contenedorCard);
-
-    // const card = document.createElement (`div`);
-    // card.classList.add (`pokemon-card`);
-
-    // const imgContenedor = document.createElement (`div`);
-    // imgContenedor.classList.add (`img-contenedor`);
-
-    // const imgPokemon = document.createElement (`img`);
-    // imgPokemon.src = pokemon.sprites.front_default
-
-    // imgContenedor.appendChild (imgPokemon);
-
-    // const contenedorNumNom = document.createElement (`div`);
-    // contenedorNumNom.classList.add (`contenedor-num-nom`);
-
-    // const numeroPokemon = document.createElement (`p`);
-    // numeroPokemon.classList.add (`numero-pokemon`);
-    // numeroPokemon.textContent = `#${pokemon.id.toString().padStart(3, 0)}`;
-
-    // const nombrePokemon = document.createElement (`p`);
-    // nombrePokemon.classList.add (`nombre-pokemon`);
-    // nombrePokemon.textContent = pokemon.name
-
-    // contenedorNumNom.appendChild (nombrePokemon);
-    // contenedorNumNom.appendChild (numeroPokemon);
-
-    // const contenedorBotonFav = document.createElement (`div`);
-    // contenedorBotonFav.classList.add  (`contenedor-botonfav`);
-    
-    // const botonFavorito = document.createElement (`button`);
-    // botonFavorito.classList.add  (`boton-favorito`);
-
-    // contenedorBotonFav.appendChild (botonFavorito);
-
-    // card.appendChild (imgContenedor);
-    // card.appendChild (contenedorNumNom);
-    // card.appendChild (contenedorBotonFav);
-
-    // const voltearCard = document.createElement (`div`);
-    // voltearCard.classList.add (`espalda-card`)
-
-    // const descripcion = document.createElement (`div`)
-    // descripcion.classList.add (`descripcion-pokemon`)
-
-    // contenedorPokemon.appendChild (descripcion)
-
-    // contenedorCard.appendChild (card);
-    // contenedorCard.appendChild (voltearCard);
-    // contenedorPokemon.appendChild (flipCard);
-
-
-    // botonFavorito.onclick = () => {
-    //     Toastify({
-    //         text: `Atrapaste un ${pokemon.name}`,
-    //         duration: 1000,
-    //         className: "info",
-    //         close: true
-    //     }).showToast ()}
-
+    function fetchPokemons (offset, limit){
+    spinner.style.display = "flex"
+    for (let i = offset; i <= offset + limit; i++){
+        fetchPokemon(i);
+    }
+}
 
 fetchPokemons(offset, limit);
 
@@ -208,7 +149,7 @@ document.addEventListener("keyup", e => {
 
         if (e.key ==="Escape")e.target.value = ""
 
-        document.querySelectorAll(".pokemon-card").forEach(fetchPokemons =>{
+        document.querySelectorAll(".carta-frente").forEach(fetchPokemons =>{
 
             fetchPokemons.textContent.toLowerCase().includes(e.target.value.toLowerCase())
             ?fetchPokemons.classList.remove("filtro")
@@ -260,13 +201,12 @@ formLogin.onsubmit = ( event ) => {
         contenedorPokemon.style.display = "flex"
         buscadorPokemon.style.display = "block"
         paginacion.style.display = "flex"
-        paginacion2.style.display = "flex"
+        // paginacion2.style.display = "flex"
     } else {        
         loginIncorrecto.style.display = "block"
         inputPass.style.border = "2px solid red"
         inputUser.style.border = "2px solid red"
         buscadorPokemon.style.display = "none"
-        
         formLogin.reset();
     }
 
